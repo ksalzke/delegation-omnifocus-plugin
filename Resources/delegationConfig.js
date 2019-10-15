0e1dd1692a00cd30957509a6d8be42218ef06841
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-10-15 22:17:26 +1100

    Run customComplete on or mark as complete original

--- Resources/delegationConfig.js
+++ Resources/delegationConfig.js
@@ -1,37 +1,53 @@
 var _ = (function() {
-	var delegationConfig = new PlugIn.Library(new Version("1.0"));
+  var delegationConfig = new PlugIn.Library(new Version("1.0"));
 
-	delegationConfig.waitingTag = () => {
-		// edit the below line to configure the tag used to mark
-		// tasks as waiting
-		// THIS SHOULD BE A TAG OBJECT
-		return tagNamed("Activity Type").tagNamed("⏳ Waiting");
-	};
+  delegationConfig.waitingTag = () => {
+    // edit the below line to configure the tag used to mark
+    // tasks as waiting
+    // THIS SHOULD BE A TAG OBJECT
+    return tagNamed("Activity Type").tagNamed("⏳ Waiting");
+  };
 
-	delegationConfig.followUpMethods = () => {
-		// edit the below line to configure the list of tags used
-		// to mark different communication methods (i.e. means of follow-up)
-		// THIS SHOULD BE AN ARRAY OF TAG OBJECTS
-		return tagNamed("Activity Type").tagNamed("Contact").children;
-	};
+  delegationConfig.followUpMethods = () => {
+    // edit the below line to configure the list of tags used
+    // to mark different communication methods (i.e. means of follow-up)
+    // THIS SHOULD BE AN ARRAY OF TAG OBJECTS
+    return tagNamed("Activity Type").tagNamed("Contact").children;
+  };
 
-	delegationConfig.defaultFollowUpMethod = () => {
-		// edit the below line to configure the default contact
-		// method that should be selected in the form
-		// THIS SHOULD BE A TAG OBJECT THAT IS INCLUDED IN THE
-		// FOLLOW UP METHODS ABOVE
-		return tagNamed("Activity Type")
-			.tagNamed("Contact")
-			.tagNamed("📧 Email");
-	};
+  delegationConfig.defaultFollowUpMethod = () => {
+    // edit the below line to configure the default contact
+    // method that should be selected in the form
+    // THIS SHOULD BE A TAG OBJECT THAT IS INCLUDED IN THE
+    // FOLLOW UP METHODS ABOVE
+    return tagNamed("Activity Type")
+      .tagNamed("Contact")
+      .tagNamed("📧 Email");
+  };
 
-	delegationConfig.uninheritedTags = () => {
-		// edit the below line to configure the list of tags that
-		// will NOT be inherited by 'follow up' or 'waiting' tasks
-		// THIS SHOULD BE AN ARRAY OF TAG OBJECTS
-		return [tagNamed("▵")];
-	};
+  delegationConfig.uninheritedTags = () => {
+    // edit the below line to configure the list of tags that
+    // will NOT be inherited by 'follow up' or 'waiting' tasks
+    // THIS SHOULD BE AN ARRAY OF TAG OBJECTS
+    return [tagNamed("▵")];
+  };
 
-	return delegationConfig;
+  delegationConfig.functionsForOriginalTaskBeforeWaiting = () => {
+    // edit the below to configure the function(s) that will be
+    // run on the original task before the 'waiting' task is created,
+    // including the action to complete the original task
+    // THIS SHOULD BE AN ARRAY OF FUNCTIONS
+    customCompletePlugin = PlugIn.find("com.KaitlinSalzke.customComplete");
+    if (customCompletePlugin !== null) {
+      return [customCompletePlugin.library("customCompleteLib").customComplete];
+    } else {
+      basicCompleteFunction = task => {
+        task.markComplete();
+      };
+      return [basicCompleteFunction];
+    }
+  };
+
+  return delegationConfig;
 })();
 _;
--- Resources/waitingFor.js
+++ Resources/waitingFor.js
@@ -1,28 +1,33 @@
 var _ = (function() {
-	var action = new PlugIn.Action(function(selection, sender) {
-		config = this.delegationConfig;
+  var action = new PlugIn.Action(function(selection, sender) {
+    config = this.delegationConfig;
 
-		// configure tags
-		waitingTag = config.waitingTag();
-		uninheritedTags = config.uninheritedTags();
+    // configure tags
+    waitingTag = config.waitingTag();
+    uninheritedTags = config.uninheritedTags();
 
-		tasks = selection.tasks;
+    tasks = selection.tasks;
 
-		tasks.forEach(task => {
-			// add "Waiting for: " to task name
-			waitingForTaskName = `Waiting for: ${task.name}`;
+    tasks.forEach(task => {
+      // do functions set up in config file first
+      config.functionsForOriginalTaskBeforeWaiting().forEach(func => {
+        func(task);
+      });
 
-			// create task and add relevant tags
-			waitingTask = new Task(waitingForTaskName, task.after);
-			waitingTask.addTag(waitingTag);
-			waitingTask.removeTags(uninheritedTags);
-		});
-	});
+      // add "Waiting for: " to task name
+      waitingForTaskName = `Waiting for: ${task.name}`;
 
-	action.validate = function(selection, sender) {
-		return selection.tasks.length >= 1;
-	};
+      // create task and add relevant tags
+      waitingTask = new Task(waitingForTaskName, task.after);
+      waitingTask.addTag(waitingTag);
+      waitingTask.removeTags(uninheritedTags);
+    });
+  });
 
-	return action;
+  action.validate = function(selection, sender) {
+    return selection.tasks.length >= 1;
+  };
+
+  return action;
 })();
 _;
